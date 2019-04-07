d1a2edbbe93ff41188b60ab3a33c10699b452767
Andrea Loriedo <el16al@leeds.ac.uk>
2019-04-07 20:12:38 +0100

    Started implementing game mode selection

--- Assets/MyScripts/DartsScripts/DartGrabbing.cs
+++ Assets/MyScripts/DartsScripts/DartGrabbing.cs
@@ -14,6 +14,7 @@ namespace Valve.VR.InteractionSystem.Sample
         public Transform attachmentPoint;
         public Transform hoverPoint;
         public GameObject prefabToGrab;
+        public Transform spawnPoint;
 
         public GameObject dart;
 
@@ -23,7 +24,8 @@ namespace Valve.VR.InteractionSystem.Sample
         // Update is called once per frame
         void Update()
         {
-           DestroyIfMiss(dart);
+            if(dart != null)
+            DestroyIfMiss(dart);
         }
 
         private void OnEnable()
@@ -49,7 +51,6 @@ namespace Valve.VR.InteractionSystem.Sample
         {
             if (newValue)
             {
-                control.Respawned(true);
                 GrabDart();
             }
         }
@@ -65,22 +66,18 @@ namespace Valve.VR.InteractionSystem.Sample
 
         void GrabDart()
         {
+            Quaternion rotationOffset = Quaternion.Euler(0, 180, 0);;
+            control.Respawned(true);
             floor.SetHit(false);
             prefabToGrab.SetActive(true);
             dart = GameObject.Instantiate<GameObject>(prefabToGrab); // Create new instance of the dart prefab
             dartCollider = GameObject.Find("Dart").GetComponent<CapsuleCollider>();
-            dartRB = dart.GetComponent<Rigidbody>();
-            dartRB.isKinematic = true;
             dartCollider.enabled = true;
-            dart.transform.position = attachmentPoint.position;
-            dart.transform.rotation = attachmentPoint.rotation;
-            DelayedGravityActivation(2, dartRB);
+            // dart.transform.position = attachmentPoint.position;
+            // dart.transform.rotation = attachmentPoint.rotation;
+            dart.transform.position = spawnPoint.position + new Vector3(0, 0.2f, 0);
+            dart.transform.rotation = spawnPoint.rotation * rotationOffset;
         }
 
-        IEnumerator DelayedGravityActivation(float delay, Rigidbody rb)
-        {
-            rb.isKinematic = false;
-            yield return new WaitForSeconds(delay);
-        }
     }
 }
---
+++ Assets/MyScripts/DartsScripts/DartsOptions.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class DartsOptions : MonoBehaviour
+    {
+        public HoverButton hoverButton;
+
+        public GameObject bullseye;
+
+        enum options {Still, Move};
+
+        private bool moveTarget;
+        public TextMeshPro moveText;
+
+        options gameMode;
+
+        private void Start()
+        {
+            hoverButton.onButtonDown.AddListener(OnButtonDown);
+            moveTarget = false;
+            moveText = GetComponent<TextMeshPro>() ?? gameObject.AddComponent<TextMeshPro>();
+            gameMode = options.Still;
+        }
+
+        void Update()
+        {
+            moveText.text = " " + ModeSelect();
+        }
+
+        private void OnButtonDown(Hand hand)
+        {
+            moveTarget = !moveTarget;
+        }
+
+        options ModeSelect()
+        {
+            if(moveTarget)
+            {
+                gameMode = options.Move;
+            }
+            else if(!moveTarget)
+            {
+                gameMode = options.Still;
+            }
+
+            return gameMode;
+        }
+    }
+}
