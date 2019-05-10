34bbb8fb6579e4f42928c182309de670b52ee874
unknown <el16al@leeds.ac.uk>
2019-05-10 21:38:33 +0100

    Fixed some bugs in link length retrieval

--- Assets/MyScripts/ArmModelScripts/ArmKinematicsController.cs
+++ Assets/MyScripts/ArmModelScripts/ArmKinematicsController.cs
@@ -3,70 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using UXF;
 
-public class ArmKinematicsController : MonoBehaviour
+namespace Valve.VR.InteractionSystem.Sample
 {
-
-    public Limb[] arm; // Array of instances of the limb struct (one element for each joint + link)
-    public float[] userLinkLength;
-
-    // UXF
-    public Session session;
-
-    // Update is called once per frame
-
-    void Start()
+    public class ArmKinematicsController : MonoBehaviour
     {
-        retrieveLinkLengths();
-    }
 
-    void retrieveLinkLengths()
-    {
-        userLinkLength[0] = (float) session.participantDetails["wrist_length"];
-        userLinkLength[1] = (float) session.participantDetails["lower_arm_length"];
-        userLinkLength[2] = (float) session.participantDetails["upper_arm_length"];
-    }
+        public Limb[] arm; // Array of instances of the limb struct (one element for each joint + link)
+        public float[] userLinkLength;
 
-    void Update()
-    {   
-        ScaleLinks(arm);
-        MoveLimb(arm);
-    }
+        // UXF
+        public Session session;
+        public ExperimentManager experiment;
 
-    void MoveLimb(Limb[] limb)
-    {
-        foreach (Limb limblink in limb)
+        // Update is called once per frame
+
+        void Start()
         {
-            Vector3 offset = limblink.previousJoint.rotation * new Vector3(0, 0, -limblink.link_length); // Move down along the arm to position joints
-            limblink.joint.position = limblink.previousJoint.position + offset; // Update position based on the position of the previous joint and the link length
-            limblink.link.position = (limblink.joint.position + limblink.previousJoint.position) / 2f; // PLace link model at midpoint between each joint
-            limblink.link.rotation = limblink.previousJoint.rotation; // Align link along the joint axes
+            retrieveLinkLengths(experiment.GetParticipantDetails());
         }
-    }
 
-    void ScaleLinks(Limb[] limb)
-    {
-        Vector3 linkLength = transform.localScale;
-
-        foreach (Limb limblink in limb)
+        void retrieveLinkLengths(ExperimentManager.ParticipantDetails participantDetails)
         {
-            foreach (float linkL in userLinkLength)
+            userLinkLength = new float[3];
+            
+            userLinkLength[0] = participantDetails.wristLength;
+            userLinkLength[1] = participantDetails.lowerArmLength;
+            userLinkLength[2] = participantDetails.upperArmLength;
+        }
+
+        void Update()
+        {   
+            ScaleLinks(arm);
+            MoveLimb(arm);
+        }
+
+        void MoveLimb(Limb[] limb)
+        {
+            foreach (Limb limblink in limb)
             {
-                linkLength.z = linkL * 5f;
+                Vector3 offset = limblink.previousJoint.rotation * new Vector3(0, 0, -limblink.link_length); // Move down along the arm to position joints
+                limblink.joint.position = limblink.previousJoint.position + offset; // Update position based on the position of the previous joint and the link length
+                limblink.link.position = (limblink.joint.position + limblink.previousJoint.position) / 2f; // PLace link model at midpoint between each joint
+                limblink.link.rotation = limblink.previousJoint.rotation; // Align link along the joint axes
             }
-            limblink.link.localScale = linkLength;
-            // limblink.link.localScale = new Vector3(limblink.link_length, 0, 0);
         }
+
+        void ScaleLinks(Limb[] limb)
+        {
+            Vector3 linkLength = transform.localScale;
+
+            foreach (Limb limblink in limb)
+            {
+                foreach (float linkL in userLinkLength)
+                {
+                    linkLength.z = linkL * 5f;
+                    limblink.link.localScale = linkLength;
+                }
+                // limblink.link.localScale = new Vector3(limblink.link_length, 0, 0);
+            }
+        }
+
+
+        [System.Serializable]
+        public struct Limb
+        {
+            public string internalName;
+            public Transform previousJoint;
+            public Transform joint;
+            public Transform link;
+            [HideInInspector]
+            public float link_length;
+        }
+
     }
-
-
-    [System.Serializable]
-    public struct Limb
-    {
-        public string internalName;
-        public Transform previousJoint;
-        public Transform joint;
-        public Transform link;
-        public float link_length;
-    }
-
 }
